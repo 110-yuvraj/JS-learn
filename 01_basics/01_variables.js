627b47bf6e9c85236b912f093cccf74fccab359a
Yuvraj Singh Solanki <93589497+110-yuvraj@users.noreply.github.com>
2023-12-25 18:06:33 +0000

    tut 2 push

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const account_Id = 205121110;
-let account_Email = "yuvi@gmail.com"
-var accountPassword = "12345"
-accountcity = "Indore"
+let account_Email = "yuvi@gmail.com";
+var accountPassword = "12345";
+accountcity = "Indore";
 
 // account_Id = 110   --> Not allowed
 
@@ -10,9 +10,15 @@ Prefer not to use var because of issue in block scope
 and functional scope
 */
 
-account_Email = "yuvirajput@gmail.com"
-accountPassword = "54321"
-accountcity = "bhopal"
+account_Email = "yuvirajput@gmail.com";
+accountPassword = "54321";
+accountcity = "bhopal";
 let accountstate;
 console.log(account_Id);
-console.table([account_Id,account_Email, accountPassword,accountcity, accountstate])
+console.table([
+  account_Id,
+  account_Email,
+  accountPassword,
+  accountcity,
+  accountstate,
+]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert('hello yuvraj')  //it will only execute in browser not here
+
+//  code should be readable so that if another person check our code
+// he can easily understand the code
+
+
+let name = "Yuvraj"
+
+let age = 24
+
+let LoggedIn = true;
+
+/*
+Number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+*/
+
+// Undefined ka type undefined hi hota hai
+
+console.log(typeof age);
+console.log(typeof "Yuvraj")
+console.log(typeof null) // null ka type object hota hai ye interview me puch skte hai

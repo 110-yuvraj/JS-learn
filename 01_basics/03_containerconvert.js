921e64fe74cf36c15bbe5bf6c25f44e161062167
Yuvraj Singh Solanki <93589497+110-yuvraj@users.noreply.github.com>
2023-12-25 18:21:15 +0000

    convert

---
+++ 01_basics/03_containerconvert.js
@@ -0,0 +1,29 @@
+// let score = 87
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//  "123" => 123
+//  "123yuvi" => NaN
+// true => 1 ; false => 0
+
+let isloggedIn = "yuvraj"
+
+let boolLogged = Boolean(isloggedIn)
+console.log(boolLogged);
+
+
+
+// 1 => true ; 0=>false;
+//  "hello" => true ; "" => false
+
+
+let someNum = 345
+
+let changeNum = String(someNum)
+
+console.log(changeNum)
+console.log(typeof changeNum)

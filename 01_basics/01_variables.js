1dce3008950f864fad07dfaf4d9d9009eed9a9de
Yuvraj Singh Solanki <93589497+110-yuvraj@users.noreply.github.com>
2023-12-26 18:00:10 +0000

    changes in variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -10,6 +10,10 @@ Prefer not to use var because of issue in block scope
 and functional scope
 */
 
+
+// Hoisting in JavaScript:
+// Hoisting is a concept that enables us to extract values of variables and functions even before initializing/assigning value without getting errors and this happens during the 1st phase (memory creation phase) of the Execution Context.
+
 account_Email = "yuvirajput@gmail.com";
 accountPassword = "54321";
 accountcity = "bhopal";

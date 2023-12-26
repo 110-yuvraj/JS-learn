03f04ef9b518ba1b51ce33425eb83601e4970fd6
Yuvraj Singh Solanki <93589497+110-yuvraj@users.noreply.github.com>
2023-12-26 17:03:45 +0000

    changes

--- 01_basics/03_containerconvert.js
+++ 01_basics/03_containerconvert.js
@@ -62,20 +62,6 @@ let gameCounter = 100;
 // console.log(gameCounter)
 
 
-// we should avoid below conversion in production environment
-
-// console.log("yuvi" < "yuvraj")
-// console.log("1" > 1)
-// console.log("02" > 1)
-// console.log(null > 0)
-// console.log(null >= 0)
-// console.log(null < 0)
 
 
-// console.log(undefined > 0)
-// console.log(undefined == 0)
-// console.log(undefined < 0)
-// console.log(undefined >= 0)
 
-//  strict check   "==="  yaha datatype bhi check hota hai
-console.log("2" === 2)
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log("yuvi" < "yuvraj")
+// console.log("1" > 1)
+// console.log("02" > 1)
+// we should avoid below conversion in production environment
+// console.log(null > 0)
+// console.log(null >= 0)
+// console.log(null < 0)
+
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+// console.log(undefined >= 0)
+
+//  strict check   "==="  yaha datatype bhi check hota hai
+console.log("2" === 2)

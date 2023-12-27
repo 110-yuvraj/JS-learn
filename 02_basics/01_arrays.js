7abe47b0054e09d40cf64405aaf1a350aa6807b3
Yuvraj Singh Solanki <93589497+110-yuvraj@users.noreply.github.com>
2023-12-27 18:19:24 +0000

    basice part 2 arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,31 @@
+// Arrays in JavaScript
+
+const myArr = [2,5,8,9,12,28]
+// console.log(myArr[4]);
+// Array can contain different values such as Numbers, boolean , Strings , objects and other Arrays as well
+//  Array methods
+// myArr.push(34);  //pushing element in the end of array
+// console.log(myArr);
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(91);   //adding element in the front
+// console.log(myArr)
+
+// myArr.shift();  //removing from front
+// console.log(myArr)
+
+// console.log(myArr.includes(9));
+
+const newArr = myArr.join();  //convert array to string 
+// console.log(typeof myArr)
+// console.log(typeof newArr)
+
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1)
+console.log("B",myArr );
+
+const myn2 = myArr.splice(1,3)  //splice original array ko modify karta hai
+console.log("c",myArr );
+console.log(myn2);

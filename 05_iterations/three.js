a2fb33514793ad6980529afe4172ca42c308c0e9
Yuvraj Singh Solanki <93589497+110-yuvraj@users.noreply.github.com>
2024-01-01 18:55:28 +0000

    loops in js

---
+++ 05_iterations/three.js
@@ -0,0 +1,39 @@
+// for of loop
+
+// ["","",""]
+
+const arr = [1,2,3,4,5];
+// for (const iterator of arr) {
+//     console.log(iterator);
+// }
+
+const greetings = "Hello world"
+// for( const it of greetings){
+//     console.log(it);
+// }
+
+//Maps
+const map = new Map()
+map.set('IN',"India");
+map.set('USA',"United states of America")
+map.set('FR',"France")
+// console.log(map);
+
+// for(const key of map){
+//     console.log(key);
+// }
+// for(const [key,value] of map){
+//     console.log(key , ':-', value);
+// }
+// for(const [key] of map){
+//     console.log(key);
+// }
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }            object is not iteratable like this 
